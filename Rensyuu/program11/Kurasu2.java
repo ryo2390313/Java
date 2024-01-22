69e1e7527293d5098b20674f3bfe47d4ba62b4ca
ryoto <149650300+ryo2390313@users.noreply.github.com>
2024-01-22 14:57:08 +0900

    Commit

--- Rensyuu/program11/Kurasu2.java
+++ Rensyuu/program11/Kurasu2.java
@@ -1,9 +1,9 @@
 package Rensyuu.program11;
 
 public class Kurasu2 {
-    class Rin {
-        private int x;
-        private int y;
+    
+        int x = 100;
+        int y = 200;
 
         void setValus(int x, int y) {
             this.x = x;
@@ -11,21 +11,16 @@ public class Kurasu2 {
         }
 
         
-        int getX() {
-            return x;
-        }
-        int getY() {
-            return y;
-        }
-
-        int sum() {
+        
+        void sum() {
             int total = 0;
             for (int i = x; i <= y; i++) {
                 total += i;
             }
-            return total;
+            System.out.println(x + "から" + y + "までの合計値は" + total + "です。");
+        
         }
-    }
+    
 }
 
  
--- Rensyuu/program11/Main3.java
+++ Rensyuu/program11/Main3.java
@@ -1,18 +1,13 @@
 package Rensyuu.program11;
-import Rensyuu.program11.Kurasu2.Rin;
+import Rensyuu.program11.Kurasu2;
 public class Main3 {
     public static void main(String[] args){
     Kurasu2 kurasu2Instance = new Kurasu2();
+    kurasu2Instance.sum();
     
-    Rin  rin = kurasu2Instance.new Rin();
     
-    int x = 100;
-    int y = 200;
-    rin.setValus(x, y);
-
-    int total = rin.sum();
-
-    System.out.println(rin.getX() + "から" + rin.getY() + "までの合計値は" + total + "です。");
+    
+    
     
  }
 }
